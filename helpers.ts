2987bba2ebbc7f0af87e3b40c81e8919e4eec557
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:47:05 +0100

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:46:45.582Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T19:46:53.262Z
+Commit: Resolve timezone issues in date handling
