fb6bffd67cd366d42f6f77b396874a5b34d728b5
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:45:45 +0100

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:45:24.281Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T19:45:32.709Z
+Commit: Fix broken links in documentation
