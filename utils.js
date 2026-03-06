e299563229b49ca2f2acba59a3bf01b0ef2d36e8
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:47:51 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:47:11.047Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T19:47:39.320Z
+Commit: Fix CSS styling bug on mobile devices
