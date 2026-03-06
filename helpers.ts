f9ed9a27f1148188aef63536a3853ae381f0c65c
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:46:38 +0100

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:46:07.110Z
-Commit: Add multi-language support
+Timestamp: 2026-03-06T19:46:25.629Z
+Commit: Resolve null pointer exception in user service
