8abaaa6d5e977af36ccf0bb8b918632f966c74d2
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:46:07 +0100

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:45:53.750Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-06T19:45:55.182Z
+Commit: Resolve null pointer exception in user service
