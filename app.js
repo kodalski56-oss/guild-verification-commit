299012a71f466fc56e2c4a850253c006538c02cc
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:48:24 +0100

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:48:02.048Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-06T19:48:11.696Z
+Commit: Fix broken links in documentation
