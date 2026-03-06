196f750e3b65ce0e04e2c0a24259711c503b11be
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:49:04 +0100

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:48:43.428Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T19:48:52.144Z
+Commit: Fix memory leak in event handlers
