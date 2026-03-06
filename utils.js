52facd76e15b51488375f46d93a1b73ac7544948
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:46:44 +0100

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:45:55.182Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-06T19:46:32.060Z
+Commit: Fix CSS styling bug on mobile devices
