e00e6565bda755aa23d3400cc7bc2b65ec146857
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:48:14 +0100

    Fix memory leak in event handlers

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:46:22.687Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-06T19:48:02.048Z
+Commit: Fix memory leak in event handlers
