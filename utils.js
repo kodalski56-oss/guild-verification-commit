f196c31bef98e4479c409ce74bb0bc533f470ac8
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:46:56 +0100

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:46:32.060Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-06T19:46:44.156Z
+Commit: Resolve null pointer exception in user service
