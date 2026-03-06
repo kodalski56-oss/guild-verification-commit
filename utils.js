5f59a40f64a62614e95c3155c58887958b502d34
kodalski56-oss <kodalski56@gmail.com>
2026-03-06 20:46:06 +0100

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T19:45:48.710Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-06T19:45:53.750Z
+Commit: Add export functionality for reports
